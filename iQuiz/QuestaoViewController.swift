fec85533434704f577fac150976d20fc132775e2
Thiago Aires da Silva <makarrao@gmail.com>
2025-03-31 07:02:09 -0300

    navigation

---
+++ iQuiz/QuestaoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestaoViewController.swift
+//  iQuiz
+//
+//  Created by Thiago Aires da Silva on 31/03/25.
+//
+
+import UIKit
+
+class QuestaoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -11,9 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configuraLayout()
     }
 
-
+    @IBOutlet weak var botaoIniciarQuiz: UIButton!
+    
+    
+    @IBAction func BotaoPressionado(_ sender: Any) {
+        print("o botao foi pressionado")
+    }
+    
+    func configuraLayout(){
+        botaoIniciarQuiz.layer.cornerRadius = 12.0
+        botaoIniciarQuiz.backgroundColor = UIColor(red: 116/255, green: 50/255, blue: 255/255, alpha: 1.0)
+    }
 }
 
